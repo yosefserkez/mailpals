9848b090015c7fbadff184737e84c544f22788d3
Yosef Serkez <yosefserkez@gmail.com>
2024-10-09 13:48:15 -0400

    fix: enable vips for Active Storage variants

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Mailpals
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_storage.variant_processor = :vips
     config.mission_control.jobs.base_controller_class = "AdminController"
   end
 end
