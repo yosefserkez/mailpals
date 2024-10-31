8c6987740ede05017b7e9847de8f16e4e4ebb4c9
orenmendelow <o.mendelow@gmail.com>
2024-10-30 20:21:50 -0400

    works cleaner now

--- app/javascript/controllers/disclosure_controller.js
+++ app/javascript/controllers/disclosure_controller.js
@@ -4,12 +4,7 @@ export default class extends Controller {
   static targets = ["content", "button", "buttonText", "icon"];
 
   toggle() {
-    this.contentTarget.classList.toggle("hidden");
-    this.iconTarget.classList.toggle("rotate-180");
-
-    const isHidden = this.contentTarget.classList.contains("hidden");
-    this.buttonTextTarget.textContent = isHidden
-      ? "Show Inactive Clubs"
-      : "Hide Inactive Clubs";
+    const isHidden = this.contentTarget.classList.toggle("hidden");
+    this.iconTarget.classList.toggle("rotate-180", !isHidden);
   }
 }
