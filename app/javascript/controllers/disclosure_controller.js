e6cac9ea7e60fd138218037e8662cc333254ed0e
orenmendelow <o.mendelow@gmail.com>
2024-10-30 20:35:06 -0400

    implemented slide animation. create new club is broken, just noticed

--- app/javascript/controllers/disclosure_controller.js
+++ app/javascript/controllers/disclosure_controller.js
@@ -4,7 +4,19 @@ export default class extends Controller {
   static targets = ["content", "button", "buttonText", "icon"];
 
   toggle() {
-    const isHidden = this.contentTarget.classList.toggle("hidden");
-    this.iconTarget.classList.toggle("rotate-180", !isHidden);
+    const content = this.contentTarget;
+    const isExpanded = content.style.height !== "0px";
+    
+    if (!isExpanded) {
+      content.style.height = `${content.scrollHeight}px`;
+    } else {
+      content.style.height = "0px";
+    }
+    
+    this.iconTarget.classList.toggle("rotate-180", !isExpanded);
+  }
+
+  connect() {
+    this.contentTarget.style.height = "0px";
   }
 }
