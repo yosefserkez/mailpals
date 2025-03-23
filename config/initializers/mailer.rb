cdbfea3a35ac5dbec9379f140569c01690c091c3
Yosef Serkez <yosefserkez@gmail.com>
2025-03-23 13:41:32 -0400

    mailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,1 +1,5 @@
-Resend.api_key = ENV['RESEND_API_KEY'] || raise('Missing RESEND_API_KEY environment variable')
+if Rails.env.production?
+  Resend.api_key = ENV.fetch('RESEND_API_KEY') { raise 'Missing RESEND_API_KEY environment variable' }
+else
+  Resend.api_key = ENV['RESEND_API_KEY'] || 'dummy_key_for_non_production'
+end
